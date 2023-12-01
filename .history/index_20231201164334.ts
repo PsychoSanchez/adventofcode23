95bf8862bc8f968ce3c9b7f2c6eb5a874fa8f269
Aleksandr Aronov <alexander.aronov@bolt.eu>
2023-12-01 17:33:26 +0100

    Removed history folder

--- .history/index_20231201164334.ts
+++
@@ -1,8 +0,0 @@
-for await (const val of Array.from({length: 25}).map((_, i) => i + 1)) {
-    const filename = `./${val}.ts`;
-    const file = Bun.file(filename);
-
-    if(await file.exists()) continue;
-
-    Bun.write(file, `console.log(${val})`);
-}
