6148213371313e152201ae3c3fee712a279d3d6d
Aronov Aleksandr <aronov.aleks@yandex.ru>
2023-12-09 15:41:52 +0100

    Added solution for day-9

--- shared/utils.ts
+++ shared/utils.ts
@@ -1,3 +1,19 @@
+import assert from "assert";
+
+export function getPuzzleInput() {
+  // day-9.ts
+  const indexOfDotTs = Bun.main.indexOf(".ts");
+  const indexOfDash = Bun.main.lastIndexOf("-", indexOfDotTs);
+  const day = Bun.main.slice(indexOfDash + 1, indexOfDotTs);
+
+  const dayNum = parseInt(day);
+
+  assert(!isNaN(dayNum));
+  assert(dayNum >= 1 && dayNum <= 25);
+
+  return Bun.file(`./data/day-${day}.data`).text();
+}
+
 export function unsafeParseInt(str: string): number {
   const num = parseInt(str);
 
